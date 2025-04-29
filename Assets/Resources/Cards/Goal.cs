b231be677b8c17add7241ee3c02ad467a82a4dfd
Eduarda <dudatessari@gmail.com>
2025-04-29 23:29:57 +0100

    goal assigment works again

--- Assets/Resources/Cards/Goal.cs
+++ Assets/Resources/Cards/Goal.cs
@@ -6,21 +6,32 @@ public class Goal
     [System.Serializable]
     public class Resources
     {
-        public int money = 0;  
+        public int money = 0;
         public int influence = 0;
         public int people = 0;
     }
 
-    // Required fields (must exist in JSON)
+    public enum TargetType { Self, Opponents }
+
+    // Required fields
     public int id;
     public string type;
     public string title;
     public string description;
-
-    // Optional fields (with default values)
+    
+    // Resource requirements
     public Resources resources = new Resources();
-    public int rounds = -1; 
+    
+    // Gameplay parameters
+    public int rounds = -1; // -1 = not applicable
+    public string target = "self"; // "self" or "opponents"
+    
+    [System.NonSerialized]
+    public bool used = false;
 
-    [System.NonSerialized] 
-    public bool used = false; 
+    // Helper property
+    public TargetType Target
+    {
+        get => target.ToLower() == "opponents" ? TargetType.Opponents : TargetType.Self;
+    }
 }
